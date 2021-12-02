276564ad1dcf1480a661868a59289dfd6b134625
Sze Ying <ting.szeying@gmail.com>
2021-12-02 21:42:47 +0800

    feat: /quotes/create can parse and log request body

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, function () {
   console.log("listening on 3000");
 });
 
 app.get("/", function (req, res) {
   res.send("Hello World");
+});
+
+app.post("/quotes/create", (req, res) => {
+  console.log(req.body);
+  res.send({ message: "Quote was created" });
 });
