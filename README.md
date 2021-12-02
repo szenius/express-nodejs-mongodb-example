d1681cd7eb3755c172aac77e551ec33008c8a602
Sze Ying <ting.szeying@gmail.com>
2021-12-02 22:11:59 +0800

    chore: set up mongodb connection

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Simple REST API with ExpressJS, MongoDB
 
+## Set up
+
+Make a copy of `.env.example` and name it `.env`. Then fill in the values of the variables.
+
 ## Development
 
 To restart server automatically during development, you can start your server by running
@@ -17,3 +21,4 @@ npm start
 ## Resources
 
 - [Setup tutorial](https://zellwk.com/blog/crud-express-mongodb/)
+- [Deploy a MongoDB database](https://www.mongodb.com/cloud/atlas)
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -14,5 +17,13 @@ app.get("/", function (req, res) {
 
 app.post("/quotes/create", (req, res) => {
   console.log(req.body);
+  const mongoClient = mongodb.MongoClient;
+  mongoClient.connect(process.env.MONGODB_URI, (err, client) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("Connected to Database");
+  });
+
   res.send({ message: "Quote was created" });
 });
