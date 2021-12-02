84c0ffc3213763f899ebfa192918613523b0b2e2
Sze Ying <ting.szeying@gmail.com>
2021-12-02 21:24:45 +0800

    feat: hello world on /

---
+++ README.md
@@ -0,0 +1,5 @@
+# A simple CRUD app with Express, NodeJS, MongoDB
+
+## Resources
+
+- [Setup tutorial](https://zellwk.com/blog/crud-express-mongodb/)
---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
